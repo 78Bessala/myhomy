0b2cba2a30e1d0047c1445d6c9029e3b5165592a
78Bessala <bessalaenogojeanleonel@gmail.com>
2025-12-16 14:57:12 +0100

    feat-mobile

--- src/composents/pages/Auth/Login/Login.jsx
+++ src/composents/pages/Auth/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../../../images/logo.svg";
+import utilitaire from "../../../../images/utilitaire.jpeg";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaApple } from "react-icons/fa";
 import { FaArrowLeftLong } from "react-icons/fa6";
@@ -8,116 +9,144 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="h-screen bg-[#A3D2FF] flex  gap-1 flex-col md:flex-row">
+    <section className="">
 
-      
-       {/* Flèche + Header bleu */}
-            <div className="bg-[#A3D2FF]">
-      
-              {/* Flèche retour */}
-              <Link to="/welcomme">
-                <div className="px-4 pt-4 text-black text-2xl cursor-pointer">
-                  <FaArrowLeftLong />
-                </div></Link>
-      
-      
-              {/* Logo */}
-              <div className="flex justify-center items-center h-56">
-                <img
-                  src={logo}
-              alt="logo"
-                  className="w-48 h-48 object-contain"
-                />
-              </div>
+      <section className="hidden md:flex  " id="util"  >
+        <div className="relative w-full h-64">
+          {/* Background */}
+          <div
+            className="w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: `url(${utilitaire})` }}
+          />
+
+          {/* Logo centré */}
+          <img
+            src={logo}
+            alt="logo"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+
+      </section>
+
+
+
+
+
+
+      <div className="h-screen bg-[#A3D2FF] flex  gap-1 flex-col md:flex-row">
+
+
+
+        <div className="bg-[#A3D2FF] md:hidden">
+
+          {/* Flèche retour */}
+          <Link to="/welcomme">
+            <div className="px-4 pt-4 text-black text-2xl cursor-pointer">
+              <FaArrowLeftLong />
             </div>
-
-      {/* Formulaire */}
-      <div className="w-full  h-full max-w-md bg-[#EBEBF2] -mt-10 rounded-t-3xl p-6">
-
-        {/* Titre */}
-        <h2 className="text-center text-3xl font-bold mb-6">se connecter</h2>
-      <div className="flex flex-col gap-4">
-        {/* Boutons de connexion */}
-        <div className="flex justify-between  m-4 mb-4 text-3xl">
-          <FcGoogle className="cursor-pointer" />
-          <FaFacebook className="text-blue-600 cursor-pointer" />
-          <FaApple className="cursor-pointer" />
-        </div>
-
-        {/* Ligne "ou avec" */}
-        <div className="flex items-center my-4">
-          {/* <hr className="flex-1  bg-gray-400"/> */}
-          <span className="flex-1 h-px bg-[#989393]"></span>
-          <span className="px-3 text-gray-500 text-sm">ou avec</span>
-          {/* <hr className="flex-1  bg-[#989393]"/> */}
-          <span className="flex-1 h-px bg-[#989393]"></span>
-        </div>
-
-   {/* section suivqnte */}
-   <div>
-<div>
-          {/* Champ email */}
-        <div className="mb-4">
-          <input
-            type="email"
-            id="email"
-            placeholder="myhomy@example.com"
-            className="w-full h-[48px] flex items-center pl-4 border border-[#989393]  rounded-[10px] focus:outline-none focus:ring-2 focus:ring-blue-400"
-            required
-          />
-        </div>
-
-
-        {/* Champ mot de passe */}
-        <div className="mb-2 relative">
-          <input
-            type="password"
-            id="password"
-            placeholder="****************"
-            className="flex items-center w-full h-[48px] pl-4 border border-[#989393] rounded-[10px] focus:outline-none focus:ring-2 focus:ring-blue-400"
-            required
-          />
-          <span className="absolute right-4 top-4 text-[#989393] cursor-pointer flex items-center"><BsEyeSlash /></span>
-        </div>
-        </div>
-        
-
-        {/* Remember + mot de passe oublié */}
-        <div className="flex justify-between items-center mb-6 m-2 text-sm">
-          <label className="flex items-center gap-2">
-            <input type="checkbox" className="rounded" />
-            <span className="text-xs">me rappeler</span>
-          </label>
-
-          <Link to="/forgot" className="text-[#203D89] text-xs font-medium hover:underline">
-            mot de passe oublié ?
           </Link>
 
+          {/* Logo */}
+          <div className="flex justify-center items-center h-56">
+            <img
+              src={logo}
+              alt="logo"
+              className="w-48 h-48 object-contain"
+            />
+          </div>
         </div>
 
-   </div>
-        
+        {/* Formulaire */}
+        <div className="w-full  h-full max-w-md bg-[#EBEBF2] -mt-10 rounded-t-3xl p-6">
 
-        {/* Bouton */}
-        <button
-          type="submit"
-          className="w-full bg-[#203D89] h-[48px]  text-base text-white py-3 rounded-[10px] hover:bg-[#1A326F] transition-colors"
-        >
-          se connecter
-        </button>
+          {/* Titre */}
+          <h2 className="text-center text-3xl font-bold mb-6">se connecter</h2>
+          <div className="flex flex-col 5173gap-4">
+            {/* Boutons de connexion */}
+            <div className="flex justify-between  m-4 mb-4 text-3xl">
+              <FcGoogle className="cursor-pointer" />
+              <FaFacebook className="text-blue-600 cursor-pointer" />
+              <FaApple className="cursor-pointer" />
+            </div>
+
+            {/* Ligne "ou avec" */}
+            <div className="flex items-center my-4">
+              {/* <hr className="flex-1  bg-gray-400"/> */}
+              <span className="flex-1 h-px bg-[#989393]"></span>
+              <span className="px-3 text-gray-500 text-sm">ou avec</span>
+              {/* <hr className="flex-1  bg-[#989393]"/> */}
+              <span className="flex-1 h-px bg-[#989393]"></span>
+            </div>
+
+            {/* section suivqnte */}
+            <div>
+              <div>
+                {/* Champ email */}
+                <div className="mb-4">
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="myhomy@example.com"
+                    className="w-full h-[48px] flex items-center pl-4 border border-[#989393]  rounded-[10px] focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    required
+                  />
+                </div>
 
 
-        {/* Texte en bas */}
-        <p className="text-[10px] mt-6 text-black text-center">
-          En continuant, vous acceptez de recevoir des appels, y compris par WhatsApp
-          ou SMS, et d’accepter les{" "}
-          <span className="underline">Conditions d’utilisation</span>.
-        </p>
+                {/* Champ mot de passe */}
+                <div className="mb-2 relative">
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="****************"
+                    className="flex items-center w-full h-[48px] pl-4 border border-[#989393] rounded-[10px] focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    required
+                  />
+                  <span className="absolute right-4 top-4 text-[#989393] cursor-pointer flex items-center"><BsEyeSlash /></span>
+                </div>
+              </div>
+
+
+              {/* Remember + mot de passe oublié */}
+              <div className="flex justify-between items-center mb-6 m-2 text-sm">
+                <label className="flex items-center gap-2">
+                  <input type="checkbox" className="rounded" />
+                  <span className="text-xs">me rappeler</span>
+                </label>
+
+                <Link to="/forgot" className="text-[#203D89] text-xs font-medium hover:underline">
+                  mot de passe oublié ?
+                </Link>
+
+              </div>
+
+            </div>
+
+
+            {/* Bouton */}
+            <button
+              type="submit"
+              className="w-full bg-[#203D89] h-[48px]  text-base text-white py-3 rounded-[10px] hover:bg-[#1A326F] transition-colors"
+            >
+              se connecter
+            </button>
+
+
+            {/* Texte en bas */}
+            <p className="text-[10px] mt-6 text-black text-center">
+              En continuant, vous acceptez de recevoir des appels, y compris par WhatsApp
+              ou SMS, et d’accepter les{" "}
+              <span className="underline">Conditions d’utilisation</span>.
+            </p>
+
+          </div>
+        </div>
 
       </div>
-      </div>
-        
-    </div>
+    </section>
+
+
   );
 };
 
--- src/composents/pages/Auth/Login/Succes.jsx
+++ src/composents/pages/Auth/Login/Succes.jsx
@@ -1,70 +1,94 @@
 import React from "react";
 import logo from "../../../../images/logo.svg";
 import Frame34 from "../../../../images/Frame 34.svg";
+import utilitaire from "../../../../images/utilitaire.jpeg";
 import { LuCircleCheckBig } from "react-icons/lu";
 import { Link } from "react-router-dom";
 
 const Succes = () => {
   return (
-    <div className="min-h-screen bg-[#EAE6F4] flex flex-col">
 
-      {/* Header bleu */}
-      <div className="bg-[#A3D2FF] flex justify-center items-center pt-10 pb-6 ">
+    <section>
+      <section  >
+        <div className="relative w-full h-64">
+          {/* Background */}
+          <div
+            className="w-full h-full bg-cover bg-center"
+            style={{ backgroundImage: `url(${utilitaire})` }}
+          />
 
-        {/* Container images */}
-        <div className="flex flex-col items-center">
-
-          {/* Logo principal */}
+          {/* Logo centré */}
           <img
             src={logo}
             alt="logo"
-            className="w-48 h-48 object-contain mb-[-12px]"
-          />
-
-          {/* Frame34 en dessous */}
-          <img
-            src={Frame34}
-            alt="frame"
-            className="w-20 h-20 object-contain"
-          />
-        </div>
-      </div>
-
-      {/* Contenu */}
-      <div className="flex flex-col items-center text-center px-6 py-10">
-
-        <h1 className="text-xl font-bold text-black mb-2">
-          Compte cree
-        </h1>
-
-        <p className="text-blue-600 text-sm mb-6">
-          Bienvenue chez Myhomy
-        </p>
-
-        {/* Icône cercle + check */}
-        <div className="flex justify-center items-center">
-          <LuCircleCheckBig
-            className="text-[#333333]"
-            style={{ width: "104px", height: "104px" }}
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           />
         </div>
 
+      </section>
 
-        <p className="text-gray-600 text-sm">
-          Votre compte Myhomy avec <br />
-          <span className="font-bold text-black text-lg">succès !!!</span>
-        </p>
+
+      <div className="min-h-screen bg-[#EAE6F4] flex flex-col">
+
+        {/* Header bleu */}
+        <div className="bg-[#A3D2FF] flex justify-center items-center pt-10 pb-6 ">
+
+          {/* Container images */}
+          <div className="flex flex-col items-center">
+
+            {/* Logo principal */}
+            <img
+              src={logo}
+              alt="logo"
+              className="w-48 h-48 object-contain mb-[-12px]"
+            />
+
+            {/* Frame34 en dessous */}
+            <img
+              src={Frame34}
+              alt="frame"
+              className="w-20 h-20 object-contain"
+            />
+          </div>
+        </div>
+
+        {/* Contenu */}
+        <div className="flex flex-col items-center text-center px-6 py-10">
+
+          <h1 className="text-xl font-bold text-black mb-2">
+            Compte cree
+          </h1>
+
+          <p className="text-blue-600 text-sm mb-6">
+            Bienvenue chez Myhomy
+          </p>
+
+          {/* Icône cercle + check */}
+          <div className="flex justify-center items-center">
+            <LuCircleCheckBig
+              className="text-[#333333]"
+              style={{ width: "104px", height: "104px" }}
+            />
+          </div>
+
+
+          <p className="text-gray-600 text-sm">
+            Votre compte Myhomy avec <br />
+            <span className="font-bold text-black text-lg">succès !!!</span>
+          </p>
+
+        </div>
+
+        {/* Bouton */}
+        <div className="px-6 mt-auto pb-10">
+          <button className="w-full bg-[#203D89] text-white py-3 rounded-[10px] text-sm font-semibold hover:bg-[#36148A] transition-colors">
+            <Link to="#">Continuer</Link>
+          </button>
+        </div>
 
       </div>
+    </section>
 
-      {/* Bouton */}
-      <div className="px-6 mt-auto pb-10">
-        <button className="w-full bg-[#203D89] text-white py-3 rounded-[10px] text-sm font-semibold hover:bg-[#36148A] transition-colors">
-          <Link to="#">Continuer</Link>
-        </button>
-      </div>
-
-    </div>
   );
 };
 
